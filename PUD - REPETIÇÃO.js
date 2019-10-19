d4eaa148f0460aad52912faf371a9e268b112616
marciovictor22 <51381814+marciovictor22@users.noreply.github.com>
2019-10-19 12:24:37 -0300

    Add files via upload

---
+++ PUD - REPETIÇÃO.js
@@ -0,0 +1,20 @@
+/*
+A = [10,8,5,11,42,2,1] //orderna o array
+Resultado:[1,2,5,8,10,11,42]
+*/
+let A = [10, 8, 5, 11, 42, 2, 1]; //orderna o array
+A.lenght
+function ordenacao(comp1) {
+    n = comp1.lenght
+
+
+    i = [0];
+
+    for (i = 0; i < n; i++) {
+        temp = i;
+
+        for (j = i; i < n; i++) {
+            
+        }
+
+    }
